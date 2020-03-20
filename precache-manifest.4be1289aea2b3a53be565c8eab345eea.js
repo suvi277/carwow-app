9170503728d850c5e8779010e0b12a447aaab9ab
Suvarna Mondal <suvarnamondal27@gmail.com>
2020-03-20 22:08:33 +0000

    Updates

---
+++ precache-manifest.4be1289aea2b3a53be565c8eab345eea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62d0e5cc0fecffe68570bb094c602ee9",
+    "url": "/carwow-app/index.html"
+  },
+  {
+    "revision": "bc0caa4a74ca588eb413",
+    "url": "/carwow-app/static/css/main.227af82c.chunk.css"
+  },
+  {
+    "revision": "db5545566f7b7bac68e7",
+    "url": "/carwow-app/static/js/2.67998d2a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/carwow-app/static/js/2.67998d2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc0caa4a74ca588eb413",
+    "url": "/carwow-app/static/js/main.60c9cdfa.chunk.js"
+  },
+  {
+    "revision": "e4c5b7ba423168376690",
+    "url": "/carwow-app/static/js/runtime-main.e7eda8bd.js"
+  }
+]);
